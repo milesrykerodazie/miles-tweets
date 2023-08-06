0398f7e87c0a5a57c81a5538540da46fa4aa01e2
Miles Ryker Odazie <milesodazie@gmail.com>
2023-08-07 00:27:31 +0100

    check

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,67 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
+// import { getToken } from "next-auth/jwt";
+// import { withAuth } from "next-auth/middleware";
+
+// const secret = process.env.NEXTAUTH_SECRET;
+
+// export default withAuth(
+//   async function middleware(req) {
+//     //get all cookies
+//     const check = await getToken({ req, secret });
+
+//     const pathname = req.nextUrl.pathname;
+
+//     const notSensitiveRoutes = ["/login", "/register"];
+//     const isAccessingNotSensitiveRoute = notSensitiveRoutes.some((route) =>
+//       pathname.startsWith(route)
+//     );
+
+//     const sensitiveRoutes = ["/home", "/notifications", "/profile"];
+//     const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+//       pathname.startsWith(route)
+//     );
+
+//     const homePath = pathname.startsWith("/home");
+
+//     // if (check !== null) {
+//     //   return NextResponse.redirect(new URL("/home", req.url));
+//     // }
+
+//     if (check !== null && isAccessingNotSensitiveRoute) {
+//       return NextResponse.redirect(new URL("/home", req.url));
+//     }
+
+//     if (check === null && isAccessingSensitiveRoute) {
+//       return NextResponse.redirect(new URL("/", req.url));
+//     }
+
+//     if (pathname === "/") {
+//       return NextResponse.redirect(new URL("/home", req.url));
+//     }
+//   },
+//   {
+//     callbacks: {
+//       async authorized() {
+//         return true;
+//       },
+//     },
+//   }
+// );
+
+// export const config = {
+//   matcher: [
+//     "/home:path*",
+//     "/notifications",
+//     "/profile/:path*",
+//     "/",
+//     "/login",
+//     "/register",
+//   ],
+// };
+
+import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
@@ -8,38 +69,24 @@ const secret = process.env.NEXTAUTH_SECRET;
 
 export default withAuth(
   async function middleware(req) {
-    //get all cookies
     const check = await getToken({ req, secret });
-
     const pathname = req.nextUrl.pathname;
 
-    const notSensitiveRoutes = ["/login", "/register"];
-    const isAccessingNotSensitiveRoute = notSensitiveRoutes.some((route) =>
-      pathname.startsWith(route)
-    );
+    const notSensitiveRoutes = ["/", "/login", "/register"];
+    const isAccessingNotSensitiveRoute = notSensitiveRoutes.includes(pathname);
 
     const sensitiveRoutes = ["/home", "/notifications", "/profile"];
-    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
-      pathname.startsWith(route)
-    );
+    const isAccessingSensitiveRoute = sensitiveRoutes.includes(pathname);
 
-    const homePath = pathname.startsWith("/home");
-
-    // if (check !== null) {
-    //   return NextResponse.redirect(new URL("/home", req.url));
-    // }
+    if (check === null && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
 
     if (check !== null && isAccessingNotSensitiveRoute) {
       return NextResponse.redirect(new URL("/home", req.url));
     }
 
-    if (check === null && isAccessingSensitiveRoute) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    if (pathname === "/") {
-      return NextResponse.redirect(new URL("/home", req.url));
-    }
+    return NextResponse.next();
   },
   {
     callbacks: {
@@ -49,14 +96,3 @@ export default withAuth(
     },
   }
 );
-
-export const config = {
-  matcher: [
-    "/home:path*",
-    "/notifications",
-    "/profile/:path*",
-    "/",
-    "/login",
-    "/register",
-  ],
-};

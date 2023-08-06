0d200e3b6378a9c7593b2053eb93322e607ffbd4
Miles Ryker Odazie <milesodazie@gmail.com>
2023-08-07 00:16:14 +0100

    check now

--- middleware.ts
+++ middleware.ts
@@ -2,43 +2,53 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
 
 const secret = process.env.NEXTAUTH_SECRET;
 
-export async function middleware(req: NextRequest, response: NextResponse) {
-  //get all cookies
-  const check = await getToken({ req, secret });
+export default withAuth(
+  async function middleware(req) {
+    //get all cookies
+    const check = await getToken({ req, secret });
 
-  const pathname = req.nextUrl.pathname;
+    const pathname = req.nextUrl.pathname;
 
-  const notSensitiveRoutes = ["/login", "/register"];
-  const isAccessingNotSensitiveRoute = notSensitiveRoutes.some((route) =>
-    pathname.startsWith(route)
-  );
+    const notSensitiveRoutes = ["/login", "/register"];
+    const isAccessingNotSensitiveRoute = notSensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    );
 
-  const sensitiveRoutes = ["/home", "/notifications", "/profile/:path*"];
-  const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
-    pathname.startsWith(route)
-  );
+    const sensitiveRoutes = ["/home", "/notifications", "/profile/:path*"];
+    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    );
 
-  const homePath = pathname.startsWith("/home");
+    const homePath = pathname.startsWith("/home");
 
-  // if (check !== null) {
-  //   return NextResponse.redirect(new URL("/home", req.url));
-  // }
+    // if (check !== null) {
+    //   return NextResponse.redirect(new URL("/home", req.url));
+    // }
 
-  if (check !== null && isAccessingNotSensitiveRoute) {
-    return NextResponse.redirect(new URL("/home", req.url));
+    if (check !== null && isAccessingNotSensitiveRoute) {
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+
+    if (check === null && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
   }
-
-  if (check === null && isAccessingSensitiveRoute) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL("/home", req.url));
-  }
-}
+);
 
 export const config = {
   matcher: [

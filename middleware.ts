8162a4a21595546f0591008115a623aff023e0e8
Miles Ryker Odazie <milesodazie@gmail.com>
2023-08-07 00:06:07 +0100

    check

--- middleware.ts
+++ middleware.ts
@@ -44,7 +44,7 @@ export async function middleware(req: NextRequest, response: NextResponse) {
 
 export const config = {
   matcher: [
-    "/home",
+    "/home:path*",
     "/notifications",
     "/profile/:path*",
     "/",
